6632cb33b4f7e9567a49eabf2104d8294f14f5e6
Yogeshwaran16 <syogeshwaran168@gmail.com>
2026-04-03 14:45:29 +0530

    Add PM2.5 prediction function using joblib model

--- aircast-aqi-predictor/src/├── predict.py
+++ aircast-aqi-predictor/src/├── predict.py
@@ -1,1 +1,16 @@
+import joblib
+import pandas as pd
 
+model = joblib.load('../models/model.pkl')
+
+def predict_pm25(temp, humidity, wind, hour, day):
+    data = pd.DataFrame([{
+        'temperature': temp,
+        'humidity': humidity,
+        'wind_speed': wind,
+        'hour': hour,
+        'day': day
+    }])
+
+    prediction = model.predict(data)[0]
+    return prediction
